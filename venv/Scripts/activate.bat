cf1c254e8682a6627b084ed793745524bf82419b
Adir <adirshit212@gmail.com>
2025-02-25 13:44:16 +0200

    Fix setup and install package

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\telco\Desktop\web-vuln-scanner && cd web-vuln-scanner\venv
+set VIRTUAL_ENV=C:\Users\telco\Desktop\VulnScanner\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
